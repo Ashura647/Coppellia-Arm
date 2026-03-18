a50012dff273be34dabf04886e3c62652d387ce2
anupakurupp-lab <anupakurupp@gmail.com>
2026-03-18 19:30:35 +0000

    changes to code, arm movement achived

--- Connection.py
+++ Connection.py
@@ -1,16 +1,20 @@
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
-
-print("Connecting to CoppeliaSim...")
-
+import time
 client = RemoteAPIClient()
 sim = client.getObject('sim')
 
-print("Connected successfully!")
-
-# Start simulation
+joint1= sim.getObject('/PArm/joint')
 sim.startSimulation()
 
-print("Simulation started")
+time.sleep(3)
 
+sim.setObjectFloatParam(joint1, sim.jointfloatparam_maxvel, 0.1)
+sim.setObjectFloatParam(joint1, sim.jointfloatparam_maxaccel, 0.1)
+sim.setJointTargetPosition(joint1, 1.57)
+
+
+time.sleep(10)
 sim.stopSimulation()
-print("Simulation stopped")
+
+
+
