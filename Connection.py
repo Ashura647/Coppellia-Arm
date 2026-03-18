efcb5739031e20c1f45cd88c83be05053c1ac9a1
anupakurupp-lab <anupakurupp@gmail.com>
2026-03-18 21:50:57 +0000

    added extra movement, fixed issue where code wasnt running

--- Connection.py
+++ Connection.py
@@ -3,17 +3,42 @@ import time
 client = RemoteAPIClient()
 sim = client.getObject('sim')
 
-joint1= sim.getObject('/PArm/joint')
+armjoint1,armjoint2,armjoint3,armjoint4,armjoint5=( 
+    sim.getObject('/PArm/joint1'),
+    sim.getObject('/PArm/joint1/joint2'),
+    sim.getObject('/PArm/joint1/joint2/joint3'),
+    sim.getObject('/PArm/joint1/joint2/joint3/joint4'),
+    sim.getObject('/PArm/joint1/joint2/joint3/joint4/joint5'),
+)
+
+
 sim.startSimulation()
 
 time.sleep(3)
 
-sim.setObjectFloatParam(joint1, sim.jointfloatparam_maxvel, 0.1)
-sim.setObjectFloatParam(joint1, sim.jointfloatparam_maxaccel, 0.1)
-sim.setJointTargetPosition(joint1, 1.57)
+sim.setObjectFloatParam(armjoint1, sim.jointfloatparam_maxvel, 0.1)
+sim.setObjectFloatParam(armjoint1, sim.jointfloatparam_maxaccel, 0.1)
+sim.setJointTargetPosition(armjoint1, -0.5)
+
+time.sleep(3)
+
+sim.setObjectFloatParam(armjoint2, sim.jointfloatparam_maxvel, 0.1)
+sim.setObjectFloatParam(armjoint2, sim.jointfloatparam_maxaccel, 0.1)
+sim.setJointTargetPosition(armjoint2, 1.4) 
+
+time.sleep(3)
+
+sim.setObjectFloatParam(armjoint3, sim.jointfloatparam_maxvel, 0.1)
+sim.setObjectFloatParam(armjoint3, sim.jointfloatparam_maxaccel, 0.1)
+sim.setJointTargetPosition(armjoint3, 1)
+
+time.sleep(3)
 
 
-time.sleep(10)
+
+
+
+time.sleep(5)
 sim.stopSimulation()
 
 
