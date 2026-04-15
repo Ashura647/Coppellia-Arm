8ac64b5b6d3bc137e0cd92e6ee3096dd92b40ad4
anupakurupp-lab <anupakurupp@gmail.com>
2026-04-15 16:16:06 +0100

    convoyer, and better efficency

--- Connection.py
+++ Connection.py
@@ -13,8 +13,21 @@ armjoint1,armjoint2,armjoint3,armjoint4,armjoint5,armjoint6=(
 )
 
 fingerjoint1 = sim.getObject('/PGripStraight/motor')
+conveyor_joint = sim.getObject('/efficientConveyor')
+sensor = sim.getObject('/proximitySensor')
 
 sim.startSimulation()
+
+# while True:
+#     #Read proximity sensor: result(0/1), distance, detectedPoint, detectedObjectHandle, normalVector
+#     res, distance, detectedPoint, detectedObjectHandle, normalVector = sim.readProximitySensor(sensor)
+#     if res > 0:
+#         # Object detected: Stop conveyor
+#         sim.setJointTargetVelocity(conveyor_joint, 0)
+#     else:
+#         # No object: Run conveyor
+#         sim.setJointTargetVelocity(conveyor_joint, 2)
+    
 
 #starting param
 sim.setObjectFloatParam(armjoint1, sim.jointfloatparam_maxvel, 0.1)
@@ -30,16 +43,16 @@ sim.setObjectFloatParam(armjoint2, sim.jointfloatparam_maxaccel, 0.1)
 
 
 
-sim.setJointTargetPosition(armjoint2, 2)
-sim.setJointTargetPosition(armjoint3, -3)
-# time.sleep(6)
-# sim.setJointTargetPosition(armjoint5, -2.5)
-# time.sleep(9)
-# sim.setJointTargetPosition(armjoint2, 1.8 )
-# time.sleep(6)
-# sim.setJointTargetPosition(fingerjoint1, -0.044)
+sim.setJointTargetPosition(armjoint2, 1.3)
+sim.setJointTargetPosition(armjoint3, 2)
+sim.setJointTargetPosition(armjoint5, -1.2)
+time.sleep(20)
+sim.setJointTargetPosition(fingerjoint1, -0.044)
+sim.setObjectFloatParam(armjoint3, sim.jointfloatparam_maxvel, 0.1)
+sim.setObjectFloatParam(armjoint3, sim.jointfloatparam_maxaccel, 0.1)
+sim.setJointTargetPosition(armjoint2, 0)
 
-time.sleep(10)
+time.sleep(30)
 sim.stopSimulation()
 
 
