ae6daa9a64b8f5b731143298ea1de29b3fd7849c
anupakurupp-lab <anupakurupp@gmail.com>
2026-04-15 23:36:07 +0100

    ARM PICKS UP ITEMMMMM 2 functions made, detection and starting postion

--- Connection.py
+++ Connection.py
@@ -13,21 +13,41 @@ armjoint1,armjoint2,armjoint3,armjoint4,armjoint5,armjoint6=(
 )
 
 fingerjoint1 = sim.getObject('/PGripStraight/motor')
-conveyor_joint = sim.getObject('/efficientConveyor')
+conveyor = sim.getObject('/efficientConveyor')
 sensor = sim.getObject('/proximitySensor')
+cube1 = sim.getObject('/Cuboid')
+gripperTip= sim.getObject('/PGripStraight/connector')
 
-sim.startSimulation()
+def startingpostion():
+    sim.setJointTargetPosition(armjoint1, 0)
+    sim.setJointTargetPosition(armjoint2, 0)
+    sim.setJointTargetPosition(armjoint3, 0)
+    sim.setJointTargetPosition(armjoint4, 0)
+    sim.setJointTargetPosition(armjoint5, 0)
+    sim.setJointTargetPosition(armjoint6, 0)
+    #sim.setJointTargetPosition(fingerjoint1, 0)
+    for _ in range(400):
+        sim.step()
 
-# while True:
-#     #Read proximity sensor: result(0/1), distance, detectedPoint, detectedObjectHandle, normalVector
-#     res, distance, detectedPoint, detectedObjectHandle, normalVector = sim.readProximitySensor(sensor)
-#     if res > 0:
-#         # Object detected: Stop conveyor
-#         sim.setJointTargetVelocity(conveyor_joint, 0)
-#     else:
-#         # No object: Run conveyor
-#         sim.setJointTargetVelocity(conveyor_joint, 2)
+def detection():
+    sim.setJointTargetPosition(armjoint2, 1.4)
+    sim.setJointTargetPosition(armjoint3, 1.77)
+    sim.setJointTargetPosition(armjoint5, -1.4)
+    for _ in range(200):
+        sim.step()
+
+    sim.setJointTargetPosition(fingerjoint1, -0.044)
+
+    for _ in range(80):
+        sim.step()
+
+    sim.setObjectParent(cube1, gripperTip, True)
+   
+
     
+ 
+sim.setStepping(True)
+sim.startSimulation()
 
 #starting param
 sim.setObjectFloatParam(armjoint1, sim.jointfloatparam_maxvel, 0.1)
@@ -41,18 +61,29 @@ sim.setObjectFloatParam(armjoint5, sim.jointfloatparam_maxaccel, 0.1)
 sim.setObjectFloatParam(armjoint2, sim.jointfloatparam_maxvel, 0.1)
 sim.setObjectFloatParam(armjoint2, sim.jointfloatparam_maxaccel, 0.1)
 
+sim.setBufferProperty(conveyor, 'customData.__ctrl__', sim.packTable({'vel': 0.017}))
+
+grasped = False
+while True:
+    detected, distance, detectedObj, _, _ = sim.readProximitySensor(sensor)
+    
+    
+    if detected and not grasped:
+        print("Object detected! Stopping conveyor")
+
+        sim.setBufferProperty(conveyor, 'customData.__ctrl__', sim.packTable({'vel': 0.0}))
+        detection()
+        for _ in range(400):
+            sim.step()
+        print(sim.getJointPosition(armjoint3))
+        startingpostion()
+        for _ in range(800):
+            sim.step()
+        break
+        
+    sim.step()
 
 
-sim.setJointTargetPosition(armjoint2, 1.3)
-sim.setJointTargetPosition(armjoint3, 2)
-sim.setJointTargetPosition(armjoint5, -1.2)
-time.sleep(20)
-sim.setJointTargetPosition(fingerjoint1, -0.044)
-sim.setObjectFloatParam(armjoint3, sim.jointfloatparam_maxvel, 0.1)
-sim.setObjectFloatParam(armjoint3, sim.jointfloatparam_maxaccel, 0.1)
-sim.setJointTargetPosition(armjoint2, 0)
-
-time.sleep(30)
 sim.stopSimulation()
 
 
