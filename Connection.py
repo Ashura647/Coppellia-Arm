a70f5a39f1b1d7fbe8d8078862dd241e39982bfb
anupakurupp-lab <anupakurupp@gmail.com>
2026-04-16 14:47:27 +0100

    FINAL
    ADDED FUNCTIONS FOR EACH BOX
    
    LOOP NOW WORKS PROPERLY
    
    PICKS AND PLACES
    
    READY FOR CAMERA

--- Connection.py
+++ Connection.py
@@ -2,6 +2,7 @@ from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 import time
 client = RemoteAPIClient()
 sim = client.getObject('sim')
+import keyboard
 
 armjoint1,armjoint2,armjoint3,armjoint4,armjoint5,armjoint6=( 
     sim.getObject('/PArm/joint1'),
@@ -43,11 +44,71 @@ def detection(n):
 
     sim.setObjectParent(n, gripperTip, True)
    
+def red(n):
+    sim.setJointTargetPosition(armjoint1, 1)
+    for _ in range(200):
+        sim.step()
 
+    sim.setJointTargetPosition(armjoint5, -1.5)
+    for _ in range(500):
+        sim.step()
+
+    sim.setJointTargetPosition(armjoint2, 1)
+    for _ in range(200):
+        sim.step()
+
+    sim.setJointTargetPosition(fingerjoint1, 0)
+    sim.setObjectParent(n, -1, True)
+    for _ in range(200):
+        sim.step()
+
+    
+
+def blue(n):
+    sim.setJointTargetPosition(armjoint1, 1.6)
+    for _ in range(200):
+        sim.step()
+
+    sim.setJointTargetPosition(armjoint5, -1.5)
+    for _ in range(500):
+        sim.step()
+    
+    sim.setJointTargetPosition(armjoint2, 1)
+    for _ in range(200):
+        sim.step()
+
+    sim.setJointTargetPosition(fingerjoint1, 0)
+    sim.setObjectParent(n, -1, True)
+    for _ in range(200):
+        sim.step()
+
+    
+
+def green(n):
+    sim.setJointTargetPosition(armjoint1, 2.2)
+    for _ in range(200):
+        sim.step()
+
+    sim.setJointTargetPosition(armjoint5, -1.5)
+    for _ in range(500):
+        sim.step()
+
+    sim.setJointTargetPosition(armjoint2, 1)
+    for _ in range(200):
+        sim.step()
+
+    sim.setJointTargetPosition(fingerjoint1, 0)
+    sim.setObjectParent(n, -1, True)
+    for _ in range(200):
+        sim.step()
+    
+   
     
  
 sim.setStepping(True)
 sim.startSimulation()
+
+print("To end, press Q")
 
 #starting param
 sim.setObjectFloatParam(armjoint1, sim.jointfloatparam_maxvel, 0.1)
@@ -65,22 +126,50 @@ sim.setBufferProperty(conveyor, 'customData.__ctrl__', sim.packTable({'vel': 0.0
 
 grasped = False
 while True:
+    
+    if keyboard.is_pressed('q'):
+        print("Stopping simulation...")
+        sim.stopSimulation()
+        break
+
     detected, distance, detectedObjloc, detectedObjHandle, two22 = sim.readProximitySensor(sensor)
     
     
     if detected and not grasped:
+        grasped = True
         print("Object detected! Stopping conveyor")
 
         sim.setBufferProperty(conveyor, 'customData.__ctrl__', sim.packTable({'vel': 0.0}))
         detection(detectedObjHandle)
-        
         for _ in range(400):
             sim.step()
-        print(sim.getJointPosition(armjoint3))
+        
         startingpostion()
-        for _ in range(800):
+        for _ in range(400):
             sim.step()
-        break
+        
+        red(detectedObjHandle)
+        for _ in range(400):
+            sim.step()
+        
+        # blue(detectedObjHandle)
+        # for _ in range(400):
+        #     sim.step()
+
+        # green(detectedObjHandle)
+        # for _ in range(400):
+        #     sim.step()
+        
+        startingpostion()
+        for _ in range(400):
+            sim.step()
+
+        sim.setBufferProperty(conveyor, 'customData.__ctrl__', sim.packTable({'vel': 0.017}))
+
+        grasped = False
+        
+    
+
         
     sim.step()
 
